d7475736b8f254f3507c28ff81f4b8f998453599
GitHub cn-ryh <GitHub 15543677192@163.com>
2023-03-24 12:42:34 +0800

    update 10 files

--- P1161.cpp
+++ P1161.cpp
@@ -1,1 +1,41 @@
 //P1161 write by cn_ryh
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    char lights[2000001];
+    for(int i = 1;i<2000001;i++)
+    {
+        lights[i] = 'a';
+    }
+    int n;
+    cin>>n;
+    for(int i = 0;i<n;i++)
+    {
+        double c;
+        int t;
+        cin>>c>>t;
+        for(int j = 1;j<=t;j++)
+        {
+            if(lights[int(floor(c*j))] == 'b')
+            {
+                lights[int(floor(c*j))] = 'a';
+            }
+            else
+            {
+                lights[int(floor(c*j))] = 'b';
+            }
+        }
+    }
+    for(int i = 1;i<2000001;i++)
+    {
+        if(lights[i] == 'b')
+        {
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
--- P1307.cpp
+++ P1307.cpp
@@ -1,1 +1,31 @@
 //P1307 write by cn_ryh
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string k;
+    cin>>k;
+    int r = k.length();
+    int l = 0;
+    if(k[0] == '-')
+    {
+        l++;
+        cout<<"-";
+    }
+    for(int i = r-1;i>l;i--)
+    {
+        if(k[i] == '0')
+        {
+            r--;
+        }
+        else{
+            break;
+        }
+    }
+    for(int i = r-1;i>=l;i--)
+    {
+        cout<<k[i];
+    }
+    return 0;
+}
