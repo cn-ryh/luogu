889aa5fb81de4106dfd115c9570ab7e793f645b6
GitHub cn-ryh <GitHub 15543677192@163.com>
2023-03-24 12:13:33 +0800

    update 5 files

--- B2043.cpp
+++ B2043.cpp
@@ -1,1 +1,30 @@
 //B2043 write by cn_ryh
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    bool k = true;
+    if(x%3 == 0)
+    {
+        cout<<3<<" ";
+        k = false;
+    }
+    if(x%5 == 0)
+    {
+        cout<<5<<" ";
+        k = false;
+    }
+    if(x%7 == 0)
+    {
+        k = false;
+        cout<<7<<" ";
+    }
+    if(k)
+    {
+        cout<<"n"<<endl;
+    }
+    
+    return 0;
+}
--- B2048.cpp
+++ B2048.cpp
@@ -1,1 +1,28 @@
 //B2048 write by cn_ryh
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    char c;
+    cin>>n>>c;
+    int res = 0;
+    res+=8;
+
+    if(n>1000)
+    {
+        if(n%500 == 0)
+        {
+            res += ((n/500)-2)*4;
+        }
+        else
+        {
+            res += ((n/500)-1)*4;
+        }
+    }
+    if(c == 'y')
+    {
+        res+=5;
+    }
+    cout<<res<<endl;
+}
