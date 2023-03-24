c129d71631d28ce7bfdc0b406d45b4926ccfed12
GitHub cn-ryh <GitHub 15543677192@163.com>
2023-03-24 12:10:12 +0800

    update 4 files

--- B2029.cpp
+++ B2029.cpp
@@ -1,1 +1,17 @@
 //B2029 write by cn_ryh
+#include <cstdio>
+using namespace std;
+int main()
+{
+	int h,r;
+	scanf("%d %d",&h,&r);
+	double v = h*3.1415926*r*r/1000.0;
+	double now = 0;
+	int res = 0;
+	while(now < 20.0)
+	{
+		now+=v;
+		res++;
+	}
+	printf("%d",res);
+}
